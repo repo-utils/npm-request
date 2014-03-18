ecd4b5cb5e25bb21656179b9839b318c388b06f3
fengmk2 <fengmk2@gmail.com>
2014-03-18 10:15:00 +0800

    try to use accept gzip encoding

--- example.js
+++ example.js
@@ -3,6 +3,8 @@ var request = require('./');
 request({
   method: 'get',
   path: 'connect/latest'
+}, {
+  registry: 'http://r.cnpmjs.org'
 }, function (err, data) {
   console.log(err, data);
 });
